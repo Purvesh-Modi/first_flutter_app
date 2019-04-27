4f8a8759f724e93c1866cee0e01344e77526c58d
Purvesh Modi <purveshmodi5@gmail.com>
2019-04-27 16:16:14 +0530

    Added Text Style

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -4,13 +4,16 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepOrangeAccent,
-      child: Text(
-        "Flight",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white),
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepOrangeAccent,
+        child: Text(
+          "Flight",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white,
+          decoration: TextDecoration.none),
+        ),
       ),
     );
   }
